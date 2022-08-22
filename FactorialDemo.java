56ab5daf97085524b96be2d284aa02257ef86fc0
yashchuri53 <107393908+yashchuri53@users.noreply.github.com>
2022-08-22 10:44:07 +0530

    Add files via upload

---
+++ FactorialDemo.java
@@ -0,0 +1,13 @@
+package com.tns.core;
+
+public class FactorialDemo {
+	public static void main(String args[]){  
+		  int i,fact=1;  
+		  int number=5; 
+		  for(i=1;i<=number;i++){    
+		      fact=fact*i;    
+		  }    
+		  System.out.println("Factorial of "+number+" is: "+fact);    
+		 }  
+
+}
---
+++ ReverseNumberDemo.java
@@ -0,0 +1,16 @@
+package com.tns.core;
+
+public class ReverseNumberDemo {
+	 public static void main(String[] args) {
+		    
+		    int num = 1234567, reversed = 0;
+
+		    for(;num != 0; num /= 10) {
+		      int digit = num % 10;
+		      reversed = reversed * 10 + digit;
+		    }
+
+		    System.out.println("Reversed Number: " + reversed);
+		  }
+
+}
